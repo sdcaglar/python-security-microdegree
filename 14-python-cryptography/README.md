3cb9c22ae229df039b90b4d232c592b85d56fbfa
Seda Çağlar <seedacaglar@gmail.com>
2022-03-24 10:10:25 +0300

    added example for string, random, secrets modules of python

--- 14-python-cryptography/README.md
+++ 14-python-cryptography/README.md
@@ -9,3 +9,12 @@
 - 03 | Conclusion
     - A database for authentication without any knowledge of the actual sensitive
       information.
+
+### 14.2 - Secure Numbers
+
+- 01 | Building blocks
+    - String, random, secrets modules of python
+- 02 | Developing the script
+    - Putting the parts together
+- 03 | Conclusion
+    - An easy to extend yet crytographically secure random password generator.
---
+++ 14-python-cryptography/random_pass.py
@@ -0,0 +1,17 @@
+import string
+import random
+import secrets
+
+length = 16
+
+specialChar = random.choice(string.punctuation)
+upperCase = random.choice(string.ascii_uppercase)
+digit = random.choice(string.digits)
+
+password = ""
+password += upperCase
+for i in range(13):
+    password += random.choice(string.ascii_letters)
+password += specialChar
+password += digit
+print("Random password {}".format(password))
